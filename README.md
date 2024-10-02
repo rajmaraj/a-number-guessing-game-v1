2aab73a89bbe9715b6a917622a31ff10b3fd7cc0
rajmaraj <rajmarajtech@gmail.com>
2024-10-02 10:15:52 -0600

    Added program info

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 TreeHouse Tech Degree: Project 1
     Hello
-    
+
+This program written in python when run will prompt a user with a welcome message to the guessing game. From there it will ask the user to guess, if lower/higher the program will let the user know, the user has unlimited guesses and the same guess can be made. If there is something other than a number then the program will throw a value error and prompt for a number. 
+
+After the user guesses the number correctly a message will pop up with how many guesses it took for that session and save the attempts. Then a stats message will calculate and print to show the mean/median/mode of the gathered attempts. 
+
+Gather a random number via random module from a list
+    random_answer = random.choice(numbers)
+
+Attempts list
+    guess_counter = []
+
+Stats function
+    def stats():
+
+Game function
+    def start_game():
+
+Try again variable 
+    try_again = input('Play again? Yes/No:  ').strip().lower()
+
+Play again variable
+    play_again = input("\nDo you want to reset and play again? Yes/No: ").strip().lower()
+
+Break loop if answers are no.
--- guessing_game_2.py
+++ guessing_game_2.py
@@ -6,7 +6,7 @@ numbers = list(range(1,21))
 guess_counter = []
 
 def stats():
-    if  len(guess_counter) > 0: # or if len(guess_counter) > 0: checks if guess counter has a value then run stats or guess_counter:
+    if  len(guess_counter) > 0: # //if bool(guess_counter)// or //if guess_counter:// checks if guess counter has a value then run stats.
         mean_attempts = statistics.mean(guess_counter)
         median_attempts = statistics.median(guess_counter)
         try:
@@ -14,7 +14,7 @@ def stats():
         except statistics.StatisticsError:
             mode_attempts = "No mode (all values unique)"
 
-        print(f"\nStats:")
+        print(f"\nStats:") # print stats of mean/median/mode statistics 
         print(f"Mean: {mean_attempts}")
         print(f"Median: {median_attempts}")
         print(f"Mode: {mode_attempts}")
@@ -24,14 +24,14 @@ def start_game():
     print("Guess a number from 1 to 20\n")
 
     while True:
-        random_answer = random.choice(numbers)
+        random_answer = random.choice(numbers) # chooses random number from list
         attempts = 0
         user_guess = None
 
         while user_guess != random_answer:
             try:
                 user_guess = int(input('Take a guess:  '))
-            except ValueError:
+            except ValueError: # shows error if not a number
                 print('Error, enter valid number')
                 continue
 
@@ -41,18 +41,18 @@ def start_game():
                 print("It's Higher. :( ")
             elif user_guess > random_answer:
                 print("It's Lower. :( ")
+        else: #can this else be removed? 
+            print(f"Got It! in {attempts} tries")
+            guess_counter.append(attempts)
 
-        print(f"Got It! in {attempts} tries")
-        guess_counter.append(attempts)
-
-        try_again = input('Play again? Yes/No:  ').strip().lower()
-        if try_again != 'yes':
-            break
+            try_again = input('Play again? Yes/No:  ').strip().lower() # asks to play again and stores in try_again
+            if try_again != 'yes': # if yes while true and return to line 27
+                break # if answer is no then stop game and run //stats// and play_again 
 
 while True:
     start_game()
     stats()
-    play_again = input("\nDo you want to reset and play again? Yes/No: ").strip().lower()
-    if play_again != 'yes':
+    play_again = input("\nDo you want to reset and play again? Yes/No: ").strip().lower() # asks to play again and stores in play_again
+    if play_again != 'yes': # if yes while true to run again
         print("Thanks for playing!")
-        break
+        break # end program if no
